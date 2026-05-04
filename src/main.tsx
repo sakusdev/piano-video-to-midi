29439cdb17ca6333eaed184d398933262ddade7b
sakusdev <105345796+sakusdev@users.noreply.github.com>
2026-05-04 16:06:22 +0900

    Use white-key auto-fit app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./AppBlob";
+import App from "./AppAutoFit";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
