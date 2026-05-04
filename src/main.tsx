bd76dbdd64d576c8ff589d1146a53d63cb005c6c
sakusdev <105345796+sakusdev@users.noreply.github.com>
2026-05-04 16:16:24 +0900

    Use black-key guarded detector

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./AppAutoFit";
+import App from "./AppBlackGuard";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
